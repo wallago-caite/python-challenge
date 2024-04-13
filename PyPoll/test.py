cd7ea46e33f55fb5b00901314dada84b2ffd0330
wallago-caite <caite.greenwood@gmail.com>
2024-04-13 16:24:06 -0500

    started over

--- PyPoll/test.py
+++ PyPoll/test.py
@@ -13,4 +13,22 @@ with open(csvpath, 'r') as csvfile:
    csv_list = list(csvReader)
    csv_header = csv_list[0]
    csv_data = csv_list[1:]
-print(csv_list)
+   total_votes = len(csv_data)
+
+
+
+
+#Results
+print("Election Results")
+print("------------------")
+
+#total vote line
+print ("Total Votes")
+print (total_votes)
+print ("-----------------")
+
+#print candidate line 
+
+# print winner line
+
+#export to txt file
