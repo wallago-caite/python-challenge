e9b94495fd23ace0b29e6fe0ae37742ce60370ad
wallago-caite <caite.greenwood@gmail.com>
2024-04-13 17:13:56 -0500

    test file updated

--- PyPoll/test.py
+++ PyPoll/test.py
@@ -15,9 +15,33 @@ with open(csvpath, 'r') as csvfile:
    csv_data = csv_list[1:]
    total_votes = len(csv_data)
 
+#set inital values for a for loop
+# we have to find the total # of votes per candidate with requires us to first
+#find vote total and pair to a candidate like we did in vba
+#find the % of the total votes for that candidate
+# find the max total number of votes for that candidate
+
+#find initial values
+candidate_votes = []
+candidate_percent = []
+candidate_location = [2] #where candidate is within the row (or rather list item)
+candidates = [] #list of  candidates
 
 
+for entry in csv_data:
+   candidate = entry[2] #candidate is at 3rd index location
+   if candidate not in candidates:
+      candidates= candidates + [candidate] #add new unique candidate to list of candidates. 
 
+#vote count for each candidate starts at 0
+votes = [0] * len(candidates)
+
+for entry in csv_data:
+   candidate = entry[2]
+   index = candidates.index(candidate)
+   votes[index] = votes[index] +1
+
+######################################################
 #Results
 print("Election Results")
 print("------------------")
@@ -27,7 +51,19 @@ print ("Total Votes")
 print (total_votes)
 print ("-----------------")
 
-#print candidate line 
+# Find % and winner -- Print the total number of votes per candidate
+max_votes = 0
+winner_list = 0
+
+for i in range(len(candidates)):
+   percentage = (votes[i] / total_votes) * 100
+   candidate_percent.append(percentage)
+   if votes[i] >max_votes:
+      max_votes = votes[i]
+      winner_list = i
+
+   print(candidates[i] + " " + str(votes[i]))
+
 
 # print winner line
 
