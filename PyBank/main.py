1759455e85655f92abe0cf9f69f41d8d45c3e4d4
wallago-caite <caite.greenwood@gmail.com>
2024-04-12 19:33:25 -0500

    got this m-fer to open yo

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,15 +3,20 @@ import os
 import csv
 
 # Define the relative path to the CSV file
-csvpath = "budget_data.csv"
+csvpath = "./budget_data.csv"
 
 # Open the CSV file
 with open(csvpath, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    csv_headers = next(csvreader)
-    print(csv_headers)
+   csvReader = csv.reader(csvfile)
+   csv_list = list(csvReader)
+
+   for row in csv_list:
 
 
-
-
-
+    # loop through and find total months
+    # total amount of profit/losses over the whole period
+    # average change over the whole period
+    #greatest increase in profits date and amount over period
+    # greatest decrease in profits date and amount over period
+    #print to terminal
+    #export a txt file with the results 
