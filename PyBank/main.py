97bbf3f432dcb9ecf2f35edbb1b4eabc3ee70efa
wallago-caite <caite.greenwood@gmail.com>
2024-04-12 19:04:49 -0500

    went back a few steps still can't find the path

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,17 +2,16 @@
 import os
 import csv
 
-# Define the file paths
-filepath = r'./PyBank/Resources/budget_data.csv'
-csv_file = 'budget_data.csv'
-output_file = 'output.txt'
-
-# Join the file paths
-csv_path = os.path.join(filepath, csv_file)
-output_path = os.path.join(filepath, output_file)
+# Define the relative path to the CSV file
+csvpath = "budget_data.csv"
 
 # Open the CSV file
-with open(csv_path, 'r') as csvfile:
+with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_headers = next(csvreader)
-    print(csv_headers)  
+    print(csv_headers)
+
+
+
+
+
